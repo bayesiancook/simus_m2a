168a845002113c97467f71ce7a5a222a6bb56fff
bayesiancook <nicolas.lartillot@gmail.com>
2019-03-11 19:46:42 +0100

    multigene postpred in postpredsimu.py

--- scripts/postpredsimu.py
+++ scripts/postpredsimu.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import re
+import runmm2a
+import datalib
 
 def single_gene_postpred(exp_folder, listname, basename, target_folder, lnl_min = -1.0, lnl_max = 0.0, burnin = 10, every = 10, shrink_posw = 1.0, shrink_dposom = 1.0, nrep = 1):
 
@@ -60,4 +62,41 @@ def single_gene_postpred(exp_folder, listname, basename, target_folder, lnl_min 
         os.system("mv {0}ppred{1}{2}_{3}.ali {4}{2}.ali".format(single_dir, basename, gene, rep, simu_dir))
         os.system("mv {0}ppred{1}{2}_{3}.trueparam {4}{2}.trueparam".format(single_dir, basename, gene, rep, simu_dir))
         os.system("mv {0}ppred{1}{2}_{3}.truesiteom {4}{2}.truesiteom".format(single_dir, basename, gene, rep, simu_dir))
+
+
+# assumes post pred has already been run
+def multi_gene_postpred(exp_folder, basename, target_folder):
+
+    exp_dir = exp_folder + "/"
+    multi_dir = exp_dir + "multigene/"
+
+    # this should normally be replicated
+    target_dir = target_folder + "/"
+    # simus in data sub-folder of target folder
+    simu_folder = target_dir + "data"
+    simu_dir = simu_folder + "/"
+
+    if os.path.exists(target_folder):
+        print("target folder ({0}) already exists".format(target_folder))
+        raise
+
+    os.system("mkdir " + target_folder)
+    os.system("mkdir " + simu_folder)
+    os.system("cp " + exp_dir + "all.tree " + target_folder)
+    os.system("cp " + exp_dir + "all.list " + target_folder)
+
+    with open(exp_dir + "all.list", 'r') as listfile:
+        gene_list = [gene.rstrip('\n').split()[0].replace(".ali","") for gene in listfile]
+
+    for gene in gene_list:
+        # open single gene alignment from original data
+        (ntaxa, nsite, original_ali) = datalib.read_phylip(exp_dir + "data/" + gene + ".ali")
+        #taxlist = [tax for (tax,seq) in original_ali.items()]
+        #print(len(taxlist))
+        #print(taxlist)
+        datalib.phy2codeml(exp_dir + "multigene/" + "ppred" + basename + "_0_" + gene + ".ali.ali", simu_dir + gene + ".ali", original_ali)
+        os.system("rm {0}ppred{1}_0_{2}.ali.ali".format(multi_dir, basename, gene))
+        os.system("mv {0}ppred{1}_0_{2}.ali.trueparam {3}{2}.trueparam".format(multi_dir, basename, gene, simu_dir))
+        os.system("mv {0}ppred{1}_0_{2}.ali.truesiteom {3}{2}.truesiteom".format(multi_dir, basename, gene, simu_dir))
+
 
