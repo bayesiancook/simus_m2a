128db777da82c74f0d67fbd9de4566c4ed9ec41f
bayesiancook <nicolas.lartillot@gmail.com>
2019-03-22 13:08:08 +0100

    fdr.py library

---
+++ scripts/fdr.py
@@ -0,0 +1,165 @@
+#! /usr/bin/python3.5
+
+import sys
+import numpy
+import os
+from scipy.stats import chi2
+
+def gene_codeml_fdr(score, truepos, outname):
+
+    ngene = len(score)
+    fromsimu = len(truepos)
+    ntrue = len([x for (gene,x) in truepos.items() if x > 0])
+    nfalse = ngene - ntrue
+
+    with open(outname + "_codeml.genefdr", 'w') as outfile:
+
+        # header 
+        if fromsimu:
+            outfile.write("{0:15s}\t{1:5s}\t{2:7s}\t{3:7s}\t{4:7s}\t{5:7s}\t{6:7s}\n".format("#gene", "dlnL", "pval", "e-fdr", "fdr", "tpr", "fpr"))
+        else:
+            outfile.write("{0:15s}\t{1:5s}\t{2:7s}\t{3:7s}\n".format("gene", "dlnL", "pval", "e-fdr"))
+
+        n = 0
+        fp = 0
+        tp = 0
+        for (gene,dlnl) in sorted(score.items(), key=lambda kv: kv[1], reverse=True):
+
+            if dlnl>0:
+                n = n + 1
+                pval = (1 - chi2.cdf(2*dlnl,1))/2
+                efdr = ngene*pval/n
+                if efdr > 1:
+                    efdr = 1
+                if fromsimu:
+                    if truepos[gene] > 0:
+                        tp = tp + 1
+                    else:
+                        fp = fp + 1
+                    # false discovery rate
+                    fdr = fp / n
+                    # sensitivity or recall
+                    tpr = tp / ntrue
+                    # 1 - specificity: false positive rate
+                    fpr = fp / nfalse
+                    outfile.write("{0:15s}\t{1:5.2f}\t{2:7.5f}\t{3:7.5f}\t{4:7.5f}\t{5:7.5f}\t{6:7.5f}\n".format(gene,dlnl,pval,efdr,fdr,tpr,fpr))
+                else:
+                    outfile.write("{0:15s}\t{1:5.2f}\t{2:7.5f}\t{3:7.5f}\n".format(gene,dlnl,pval,efdr))
+
+def gene_fdr(cutoff_list, score, truepos, outname):
+
+    ngene = len(score)
+    fromsimu = len(truepos)
+    ntrue = len([x for (gene,x) in truepos.items() if x > 0])
+    nfalse = ngene - ntrue
+
+    gene_ndisc = dict()
+    gene_fp = dict()
+    gene_efdr = dict()
+    gene_etpr = dict()
+    for cutoff in cutoff_list:
+        gene_ndisc[cutoff] = 0
+        gene_fp[cutoff] = 0
+        gene_efdr[cutoff] = 0
+        gene_etpr[cutoff] = 0
+
+    with open(outname + ".genefdr", 'w') as outfile:
+
+        # header
+        if fromsimu:
+            outfile.write("{0:15s}\t{1:7s}\t{2:7s}\t{3:7s}\t{4:7s}\t{5:7s}\t{6:7s}\n".format("#gene", "pp", "e-fdr", "e-tpr", "fdr", "tpr", "fpr"))
+        else:
+            outfile.write("{0:15s}\t{1:7s}\t{2:7s}\t{3:7s}\n".format("gene", "pp", "e-fdr", "e-tpr"))
+
+        # estimated total number of positively selected genes
+        etotp = sum([pp for (gene,pp) in score.items()])
+
+        fp = 0
+        tp = 0
+        n = 0
+        totpp = 0
+
+        for (gene,pp) in sorted(score.items(), key=lambda kv: kv[1], reverse=True):
+            n = n + 1
+            # total post prob for positive selection thus far: estimated number of true positives in selected set
+            totpp = totpp + pp
+            # estimated rate of false discovery: 1 - FP / N
+            efdr = 1 - totpp/n
+            # estimated sensitivity (true positive rate): TP / TOT NUMBER OF POS SEL GENES
+            etpr = totpp / etotp
+
+            if fromsimu:
+                if truepos[gene] > 0:
+                    tp = tp + 1
+                else:
+                    fp = fp + 1
+                # false discovery rate
+                fdr = fp / n
+                # sensitivity or recall
+                tpr = tp / ntrue
+                # 1 - specificity: false positive rate
+                fpr = fp / (ngene - ntrue)
+                outfile.write("{0:15s}\t{1:7.5f}\t{2:7.5f}\t{3:7.5f}\t{4:7.5f}\t{5:7.5f}\t{6:7.5f}\n".format(gene,score[gene],efdr,etpr,fdr,tpr,fpr))
+            else:
+                outfile.write("{0:15s}\t{1:7.5f}\t{2:7.5f}\t{3:7.5f}\n".format(gene,score[gene],efdr,etpr))
+
+            for cutoff in cutoff_list:
+                if pp > cutoff:
+                    gene_ndisc[cutoff] = n
+                    gene_fp[cutoff] = fp
+                    gene_efdr[cutoff] = efdr
+                    gene_etpr[cutoff] = etpr
+
+    return [gene_ndisc, gene_fp, gene_efdr, gene_etpr]
+
+
+def method_gene_fdr(cutoff_list, namelist, score, truepos, outname):
+
+    ngene = len(score)
+    fromsimu = len(truepos)
+    ntrue = len([x for (gene,x) in truepos.items() if x > 0])
+    nfalse = ngene - ntrue
+
+    gene_ndisc = dict()
+    gene_fp = dict()
+    gene_efdr = dict()
+    gene_etpr = dict()
+
+    for name in namelist:
+        [gene_ndisc[name], gene_fp[name], gene_efdr[name], gene_etpr[name]] = gene_fdr(cutoff_list, score[name], truepos, outname + "_" + name);
+    with open(outname + ".genefdr", 'w') as outfile:
+
+        outfile.write("{0:>18s}".format(""))
+        for cutoff in cutoff_list:
+            if fromsimu:
+                outfile.write("{0:^29.2f}".format(cutoff))
+            else:
+                outfile.write("{0:^19.2f}".format(cutoff))
+        outfile.write("\n")
+
+        outfile.write("{0:>18s}".format(""))
+        for cutoff in cutoff_list:
+            if fromsimu:
+                outfile.write("  {0:>5s} {1:>5s} {2:>5s} {3:>5s} {4:>5s}".format("disc", "efdr", "fdr", "etpr", "tpr"))
+            else:
+                outfile.write("  {0:>5s} {1:>5s} {2:>5s}".format("disc", "efdr", "etpr"))
+        outfile.write("\n")
+
+        for name in namelist:
+
+            if name != "codeml":
+                outfile.write("{0:>18s}".format(name))
+
+                for cutoff in cutoff_list:
+                    ndisc = gene_ndisc[name][cutoff]
+                    if ndisc:
+                        efdr = gene_efdr[name][cutoff]
+                        etpr = gene_etpr[name][cutoff]
+                        if fromsimu:
+                            fdr = gene_fp[name][cutoff] / ndisc
+                            tpr = (ndisc - gene_fp[name][cutoff]) / ntrue
+                            outfile.write("  {0:5d} {1:5.2f} {2:5.2f} {3:5.2f} {4:5.2f}".format(ndisc, efdr, fdr, etpr, tpr))
+                        else:
+                            outfile.write("  {0:5d} {1:5.2f} {2:5.2f}".format(ndisc, efdr, etpr))
+
+            outfile.write("\n")
