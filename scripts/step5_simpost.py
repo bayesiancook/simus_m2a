86ff7e6ecf47aa62af754c382f5bccd2402c17b7
bayesiancook <nicolas.lartillot@gmail.com>
2019-03-22 13:06:35 +0100

    step5: removed specific burnin parameters (default values are used)

--- scripts/step5_simpost.py
+++ scripts/step5_simpost.py
@@ -17,15 +17,15 @@ full_multi_basename = ["indmm2a", "shrunkenmm2a", "sharedmm2a", "unconsindmm2a",
 print("single gene analyses")
 for simu in ["simu30", "simu10", "simu03"]:
     print(simu)
-    m2a_postanalysis(exp_dir + simu, single_basename, [], outname = res_dir + "single" + simu, burnin = 100)
+    m2a_postanalysis(exp_dir + simu, single_basename, [], outname = res_dir + "single" + simu)
 
 print("extended multi gene analyses")
 for simu in ["simu30", "simu10", "simu03", "simu30_shrink_dposom03", "simu30_shrink_dposom01", "simu30_shrink_posw03", "simu30_shrink_posw01"]:
     print(simu)
-    m2a_postanalysis(exp_dir + simu, [], full_multi_basename, outname = res_dir + "multi" + simu, burnin = 100)
+    m2a_postanalysis(exp_dir + simu, [], full_multi_basename, outname = res_dir + "multi" + simu)
 
 print("restricted multi gene analyses")
 for simu in ["simushared", "simushrunken"]:
     print(simu)
-    m2a_postanalysis(exp_dir + simu, [], multi_basename, outname = res_dir + simu, burnin = 100)
+    m2a_postanalysis(exp_dir + simu, [], multi_basename, outname = res_dir + simu)
 
