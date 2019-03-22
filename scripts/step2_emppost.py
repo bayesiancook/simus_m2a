d0a688386c026d974d88c1e42302650c7324b5d7
bayesiancook <nicolas.lartillot@gmail.com>
2019-03-22 13:03:36 +0100

    step2: in results folder (created if not yet there)

--- scripts/step2_emppost.py
+++ scripts/step2_emppost.py
@@ -7,11 +7,16 @@ from postanalysis import *
 
 exp_folder = sys.argv[1]
 emp_folder = exp_folder + "/empirical"
+res_folder = exp_folder + "/results"
+
+if not os.path.exists(res_folder):
+    print("creating results folder")
+    os.system("mkdir " + res_folder)
 
 # post analysis
 
 single_basename = ["uninfm2a", "uninfpi50m2a", "uninfpi10m2a", "uninfpi02m2a", "subjpi50m2a", "subjpi10m2a", "subjpi02m2a"]
 multi_basename = ["indmm2a", "shrunkenmm2a", "sharedmm2a", "unconsindmm2a", "unconsshrunkenmm2a", "uninfindmm2a", "uninfshrunkenmm2a"]
 
-m2a_postanalysis(emp_folder, single_basename, multi_basename, outname = exp_folder + "/results/emp", burnin = 100)
+m2a_postanalysis(emp_folder, single_basename, multi_basename, outname = exp_folder + "/results/emp")
 
