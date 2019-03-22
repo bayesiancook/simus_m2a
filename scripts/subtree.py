e72afeddce633cd5dc6a73b66af8463003ecb0be
bayesiancook <nicolas.lartillot@gmail.com>
2019-03-22 13:07:13 +0100

    added subtree.py

---
+++ scripts/subtree.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3.5
+from ete3 import Tree
+import sys
+
+if len(sys.argv) == 1:
+    print("subtree.py treefile taxfile outfile")
+    sys.exit()
+
+treefilename = sys.argv[1]
+taxfilename = sys.argv[2]
+outfilename = sys.argv[3]
+
+tree = Tree(treefilename)
+with open(taxfilename, 'r') as taxfile:
+    taxlist = [line.rstrip('\n') for line in taxfile]
+
+tree.prune(taxlist)
+
+tree.write(outfile=outfilename,format=9)
+
+
+
